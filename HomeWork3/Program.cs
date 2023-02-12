d7ce016caf34630f1f32645760afa42e02345c04
Marina Barinova <marina.alive888@yandex.ru>
2023-02-12 19:51:03 +0300

    Задача 3(23)

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 3(23). Напишите программу, которая принимает за вход
+// число (N) и выдает таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void Quards(int uN)
+{
+    int current = 1;
+    while (current <= uN)
+    {  
+        int res = current * current * current; 
+    
+        Console.Write ($" {res},"); 
+
+        current++;
+
+    }
+
+    Console.WriteLine(" \b\b ");
+}
+Console.Write("Input your  number:  ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+if (user_number >= 1)
+{
+   Console.Write($"{user_number} -> "); 
+   Quards(user_number);
+}
+
+else 
+Console.Write("It's impossible value!");
